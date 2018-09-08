6f882fe588844b0ae774ab8e31ea9c98b1be6aa6
José Lerma <lalo_pichu22@hotmail.com>
2018-09-08 02:06:29 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -19,7 +19,6 @@ def won?(board)
   WIN_COMBINATIONS.each do |combinations|
     if (board[combinations[0]] == 'X' && board[combinations[1]] == 'X' && board[combinations[2]] == 'X') || (board[combinations[0]] == 'O' && board[combinations[1]] == 'O' && board[combinations[2]] == 'O')
       return combinations
-      break
     end
   end
   return false
